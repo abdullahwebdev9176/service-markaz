840112814f4a7d056bf790d275547b51bda7c2bb
Muhammad Abdullah <abdullah.webdev9176@gmail.com>
2026-04-04 14:56:37 +0500

    updated logo

--- src/data/assets.js
+++ src/data/assets.js
@@ -1,5 +1,9 @@
 import profilePicture from '../../public/images/profile.webp';
+import official_logo from '../../public/images/service-markaz-logo.png';
+import footer_logo from '../../public/images/footer-logo.png';
 
 export const images = {
   profile_picture: profilePicture,
+  official_logo: official_logo,
+  footer_logo: footer_logo,
 }
